589c82ed3079c65271033e41b270dc3035d9e384
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-07-24 17:57:54 +0430

    now you can see shoppingCard

--- assest/main.js
+++ assest/main.js
@@ -1,17 +1,48 @@
 // variable
-const courses = document.querySelector('#shopping-cart')
+const clothes = document.querySelector('#shopping-cart')
+      shoppingCardContent = document.querySelector('#cart-content tbody')
+      showDeleteTable = document.querySelector('.Delete')
 
 // eventListeners
 evenetListeners()
 function evenetListeners() {
-  courses.addEventListener('click', buyCourses)
+  clothes.addEventListener('click', buyCloth)
 }
 
 // functions
-function buyCourses(e) {
+function buyCloth(e) {
   e.preventDefault()
   if (e.target.classList.contains('add-to-card')) {
-    const course = e.target.parentElement
-    console.log(course);
+    const clothInformation = e.target.parentElement.parentElement
+    clothInfo(clothInformation)
   }
+}
+
+function clothInfo(cInfo) {
+  const cloth = {
+    image: cInfo.querySelector('img').src,
+    title: cInfo.querySelector('h4').textContent,
+    price: cInfo.querySelectorAll('p')[2].textContent,
+    id: cInfo.querySelector('a').getAttribute('data-id')
+  }
+  addToCard(cloth)
+}
+
+function addToCard(cInfo) {
+  let row = document.createElement('tr')
+  row.innerHTML = `
+  <tr>
+    <td>
+      <img src=${cInfo.image} width="70px">
+    </td>
+    <td>${cInfo.title}</td>
+    <td>${cInfo.price}</td>
+    <td>
+      <a href="#" class="remove" data-id="${cInfo.id}">X</a>
+    </td>
+  </tr>
+  `
+  showDeleteTable.style.display = 'block'
+  shoppingCardContent.appendChild(row)
+  console.log(row);
 }
